5a5ba494f6014f081133e4487aa65cbd9a21ed44
pohljo85440 <pohljo85440@th-nuernberg.de>
2023-07-12 19:20:19 +0200

    initial

--- src/main/java/ohm/softa/a13/tweets/TrumpTweetStats.java
+++ src/main/java/ohm/softa/a13/tweets/TrumpTweetStats.java
@@ -3,29 +3,40 @@ package ohm.softa.a13.tweets;
 import ohm.softa.a13.model.Tweet;
 import org.apache.commons.lang3.NotImplementedException;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TrumpTweetStats {
 
+	private static final Pattern ALPHA_NUMERICAL = Pattern.compile("[a-z0-9@]+");
+
     public static Map<String, Long> calculateSourceAppStats(Stream<Tweet> tweetStream) {
-        /* TODO group the tweets by the `sourceApp` they were created with and count how many it were per `sourceApp` */
-        throw new NotImplementedException("TrumpTweetStats.calculateSourceAppStats(...) not implemented yet.");
-    }
+	return tweetStream.map(t -> t.getSourceApp()).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+	}
 
     public static Map<String, Set<Tweet>> calculateTweetsBySourceApp(Stream<Tweet> tweetStream) {
-        /* TODO group the tweets by the `sourceApp`
-         * collect the tweets in `Set`s for each source app */
-        throw new NotImplementedException("TrumpTweetStats.calculateTweetsBySourceApp(...) not implemented yet.");
+		return tweetStream.collect(Collectors.groupingBy(Tweet::getSourceApp, Collectors.toSet()));
     }
 
     public static Map<String, Integer> calculateWordCount(Stream<Tweet> tweetStream, List<String> stopWords) {
-        /* Remark: implement this method at last */
-        /* TODO split the tweets, lower them, trim them, remove all words that are in the `stopWords`,
-         * reduce the result to a Map<String, Integer> to count how often each word were in the tweets
-         * optionally you could filter for all words that were used more than 10 times */
-        throw new NotImplementedException("TrumpTweetStats.tweetStream(...) not implemented yet.");
-    }
+		return tweetStream
+			.map(t -> t.getText())
+			.map(t -> t.split("( )+"))
+			.flatMap(a -> Arrays.stream(a))
+			.map(String::toLowerCase)
+			.filter(word -> ALPHA_NUMERICAL.matcher(word).matches())
+			.filter(f -> !stopWords.contains(f)) // .filter(w -> stopWords.stream().noneMatch(sw -> sw.equals(w)))
+			.reduce(new LinkedHashMap<String, Integer>(), (acc, word) -> {
+				acc.put(word, acc.compute(word, (k, v) -> v == null ? 1 : v + 1));
+				return acc;
+			}, (m1, m2) -> m1)
+			.entrySet()
+			.stream()
+			.filter(e -> e.getValue() > 10)
+			.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+	}
 }
--- src/main/java/ohm/softa/a13/tweets/TweetStreamGenerator.java
+++ src/main/java/ohm/softa/a13/tweets/TweetStreamGenerator.java
@@ -1,17 +1,21 @@
 package ohm.softa.a13.tweets;
 
+import com.google.gson.Gson;
 import ohm.softa.a13.model.Tweet;
 import org.apache.commons.lang3.NotImplementedException;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class TweetStreamGenerator {
+	private List<Tweet> tweets;
+
 	private TweetStreamGenerator(List<Tweet> tweets) {
 		this.tweets = tweets;
 	}
-
-	private List<Tweet> tweets;
 
 	public Stream<Tweet> getTweetStream() {
 		return tweets.stream();
@@ -23,9 +27,8 @@ public class TweetStreamGenerator {
 	 * @return
 	 */
 	public static TweetStreamGenerator fromJson(String path) {
-		// TODO use gson to read in the tweets from the referenced path
-		// use the getResourceAsStream method to access files in the `resources` directory
-
-		throw new NotImplementedException("Not implemented");
+		Reader reader = new InputStreamReader(TweetStreamGenerator.class.getResourceAsStream(path));
+		Gson gson = new Gson();
+		return new TweetStreamGenerator(List.of(gson.fromJson(reader, Tweet[].class)));
 	}
 }
